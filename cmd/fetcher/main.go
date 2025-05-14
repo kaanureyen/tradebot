7f4d39bc2db723a1d5ac582c80998240fcf10a4c
Kaan Üreyen <kaanureyen07@gmail.com>
2025-05-14 21:00:00 +0300

    Initial version of fetcher

--- cmd/fetcher/main.go
+++ cmd/fetcher/main.go
@@ -1,7 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+
+	binance_connector "github.com/binance/binance-connector-go"
+)
+
+const (
+	RedisAddress = "localhost:6379"
+	RedisChannel = "binance:trade:btcusdt"
+)
+
+type TradeDatePrice struct {
+	TradeDate int64
+	Price     string
+}
+
+var rdb = redis.NewClient(&redis.Options{
+	Addr: RedisAddress,
+})
+var ctx = context.Background()
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds) // show line number in logs, show microseconds
+	log.SetPrefix("[fetcher] ")
+
+	// Create a channel to listen for signals from the OS for graceful shutdown
+	shutdownSignals := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignals, syscall.SIGINT, syscall.SIGTERM)
+
+	// Create a channel to signal when to disconnect from Binance
+	disconnectFromBinance := make(chan struct{})
+	go func() {
+		<-shutdownSignals
+		fmt.Println()
+		log.Println("Received shutdown signal, will quit.")
+		disconnectFromBinance <- struct{}{} // tell Binance to stop
+	}()
+	stayConnectedToBinance("BTCUSDT", disconnectFromBinance, handleTradeEvent, handleErrorEvent)
+	log.Println("Exiting...")
+}
+
+func handleTradeEvent(event *binance_connector.WsTradeEvent) {
+	data, err := json.Marshal(TradeDatePrice{event.TradeTime, event.Price})
+	if err != nil {
+		log.Println("Error marshalling data.\nerr:", err, "\ndata:", data)
+	}
+
+	err = rdb.Publish(ctx, RedisChannel, data).Err()
+	if err != nil {
+		log.Println("Redis Publish error:", err)
+	}
+
+}
+
+func handleErrorEvent(err error) {
+	log.Println("Error in Websocket stream:", err)
+}
+
+func stayConnectedToBinance(exchange string, quit chan struct{}, handleTradeEvent func(*binance_connector.WsTradeEvent), handleErrorEvent func(error)) {
+	const timeBeforeReconnect = 5 * time.Second // 300 connections per 5 minutes is the limit. this should be fine
+	const timeoutBeforeReturn = 5 * time.Second // arbitrary. gets one <1ms, I don't think it's over network
+	for {
+		// connect to Binance Trade Websocket streamclosing Binance connection
+		websocketStreamClient := binance_connector.NewWebsocketStreamClient(false)
+		doneCh, stopCh, err := websocketStreamClient.WsTradeServe(exchange, handleTradeEvent, handleErrorEvent)
+		if err != nil {
+			log.Println("Error while opening Websocket stream:", err)
+			log.Println("Retrying in:", timeBeforeReconnect)
+			time.Sleep(timeBeforeReconnect) // wait before retrying
+			continue                        // retry
+		}
+
+		// Wait for the WS stream to close OR quit signal
+		select {
+		case <-doneCh: // Binance is done, but we are not
+			log.Println("Binance connection closed, reconnecting in:", timeBeforeReconnect)
+			time.Sleep(timeBeforeReconnect)
+			continue // reconnect
+
+		case <-quit: // we are done
+			log.Println("Telling Binance to quit.")
+			stopCh <- struct{}{}
+			log.Println("Waiting for Binance to close connection.")
+
+			// Wait for Binance to close connection OR timeout
+			select {
+			case <-doneCh:
+				log.Println("Binance connection is closed normally.")
+
+			case <-time.After(timeoutBeforeReturn):
+				log.Println("Timeout (", timeoutBeforeReturn, ") waiting for Binance to close connection")
+			}
+			return
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/kaanureyen/tradebot
 
 go 1.24.3
+
+require (
+	github.com/binance/binance-connector-go v0.8.0
+	github.com/redis/go-redis/v9 v9.8.0
+)
+
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
